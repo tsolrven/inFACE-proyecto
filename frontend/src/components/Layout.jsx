8f7341a8ba3487d90f434d14a8b2a541292fa53d
tsolrven <vicaria.silvana@gmail.com>
2026-07-04 23:06:04 -0400

    Modificación del scrollbar global (Layout.jsx y index.css)

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,12 +4,9 @@ import Sidebar from './Sidebar';
 
 export default function Layout() {
   return (
-    <div className='flex min-h-screen flex-col bg-neutral-950 text-neutral-100'>
+    <div className='flex h-screen flex-col bg-neutral-950 text-neutral-100'>
       <Navbar />
-      <div
-        className='flex flex-1'
-        style={{ height: 'calc(100vh - 54px)' }}
-      >
+      <div className='flex flex-1 overflow-hidden'>
         <Sidebar />
         <main className='flex-1 overflow-y-auto bg-neutral-950'>
           <Outlet />
