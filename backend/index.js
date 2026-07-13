416f6421c7a69f2ca38dcf58069e6f4443e702f7
tsolrven <vicaria.silvana@gmail.com>
2026-07-13 04:07:46 -0400

    Cambios para subida a producción.

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,9 @@ const app = express();
 app.use(helmet());
 
 // middlewares de parseo
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+//app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+//para subida a producción:
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
