fb449f5ec885a468b55e5ceb552605cb013c5b1b
Valentina Johana Cecilia Martínez Hitschfeld <valentina.martinez2302@alumnos.ubiobio.cl>
2026-07-14 03:42:11 -0400

    Ajustes para que todo funcione en el docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    preview: {port:443, host:true}, //puerto para el frontend, si no (5173)
 });
